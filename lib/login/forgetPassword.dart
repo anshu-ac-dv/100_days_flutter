19017652997c84e0dec15f9678c15f39a2ea4f00
anshu <anshu.ac.dv@gmail.com>
2026-01-02 18:52:51 +0530

    Initial Commit

--- lib/login/forgetPassword.dart
+++ lib/login/forgetPassword.dart
@@ -14,15 +14,47 @@ class _ForgetpasswordState extends State<Forgetpassword> {
     return Scaffold(
       body: SingleChildScrollView(
         child: Padding(
-          padding: EdgeInsets.only(top: 80, left: 20),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.start,
+          padding: EdgeInsets.only(top: 120, left: 20, right: 20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Text(
                 "Forget Your Password",
                 style: GoogleFonts.oswald(
                   fontSize: 25,
                   fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(height: 5),
+              Text("Forget your password using email."),
+              SizedBox(height: 20),
+              Form(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    TextFormField(
+                      decoration: InputDecoration(
+                        hintText: "Enter Your Email",
+                        prefixIcon: Icon(Icons.email),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 20),
+                    ElevatedButton(
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.amber,
+                        elevation: 0,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        fixedSize: Size(430, 50),
+                      ),
+                      child: Text("Get Otp"),
+                    ),
+                  ],
                 ),
               ),
             ],
